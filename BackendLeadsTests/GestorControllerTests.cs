ae0aa245098a275354d4e18f6a54208a2884c0d4
Genilson Souza <genilson.souza@sesims.com.br>
2024-07-23 07:55:57 -0400

    melhoria de codigo

---
+++ BackendLeadsTests/GestorControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using BackendLeads.Controllers;
+using BackendLeads.Services.Interface;
+using BackendLeads.DTO;
+using BackendLeads.Utils;
+using System.Threading.Tasks;
+
+public class GestorControllerTests
+{
+    private readonly Mock<IGestorService> _mockGestorService;
+    private readonly GestorController _controller;
+
+    public GestorControllerTests()
+    {
+        _mockGestorService = new Mock<IGestorService>();
+        _controller = new GestorController(_mockGestorService.Object);
+    }
+
+    [Fact]
+    public async Task Cadastrar_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        var gestorDto = new GestorDto { /* Inicialize as propriedades conforme necessário */ };
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockGestorService.Setup(service => service.AdicionarGestor(gestorDto)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.Cadastrar(gestorDto);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+}
---
+++ BackendLeadsTests/LeadsControllerTests.cs
@@ -0,0 +1,125 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using BackendLeads.Controllers;
+using BackendLeads.Service.Interface;
+using BackendLeads.DTO;
+using System.Threading.Tasks;
+using BackendLeads.Utils;
+
+public class LeadsControllerTests
+{
+    private readonly Mock<ILeadsService> _mockLeadsService;
+    private readonly LeadsController _controller;
+
+    public LeadsControllerTests()
+    {
+        _mockLeadsService = new Mock<ILeadsService>();
+        _controller = new LeadsController(_mockLeadsService.Object);
+    }
+
+    [Fact]
+    public async Task Cadastrar_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        var leadsDto = new LeadsDto { /* Inicialize as propriedades conforme necessário */ };
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.AdicionarLeads(leadsDto)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.Cadastrar(leadsDto);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task ListarLeads_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.BuscarTodosLeads()).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.ListarLeads();
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task ListarLeadsId_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        int id = 1;
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.BuscarPorId(id)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.ListarLeadsId(id);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task ListarLeadsCpf_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        string cpf = "12345678900";
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.BuscarPorCpf(cpf)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.ListarLeadsCpf(cpf);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task AtualizarLeads_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        int id = 1;
+        var leadsAtualizarDto = new LeadsAtualizarDto { /* Inicialize as propriedades conforme necessário */ };
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.AtualizarLeads(leadsAtualizarDto, id)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.AtualizarLeads(leadsAtualizarDto, id);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task DeletarLeads_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        int id = 1;
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.ApagarLeads(id)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.DeletarLeads(id);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+
+    [Fact]
+    public async Task VerificarCpfLead_ReturnsRespostaPadrao()
+    {
+        // Arrange
+        string cpf = "12345678900";
+        var respostaEsperada = new RespostaPadrao { /* Inicialize as propriedades conforme necessário */ };
+        _mockLeadsService.Setup(service => service.VerificarCpfExistente(cpf)).ReturnsAsync(respostaEsperada);
+
+        // Act
+        var result = await _controller.VerificarCpfLead(cpf);
+
+        // Assert
+        Assert.Equal(respostaEsperada, result);
+    }
+}
